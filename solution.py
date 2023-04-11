e6c54d1bb2b7d1cd86faf0676dbd496513af25bb
cheba1 <113043236+cheba1@users.noreply.github.com>
2023-04-11 15:54:25 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,19 @@
 import pandas as pd
 import numpy as np
-from scipy import stats
+
+from scipy.stats import permutation_test
 
 
 chat_id = 725861714 # Ваш chat ID, не меняйте название переменной
 
-def solution(control: List[float], experiment: List[float]) -> bool:
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    
     alpha = 0.05
-    _, p_value = stats.ttest_ind(control, experiment, equal_var=False)
-    return p_value < alpha
+    effect = permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
+                 vectorized=True, 
+                 n_resamples=5000,
+                 alternative='greater').pvalue < alpha
+    return effect # Ваш ответ, True или False
